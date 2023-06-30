cb0c09dca6f372c6a15815617e1cbc0976f0c00a
devanohdevan <devangaddi@gmail.com>
2023-06-30 10:43:14 -0700

    added photos and more scripts

--- CIS233DA-main/week 5/finalProject.zip/weatherapi.js
+++ CIS233DA-main/week 5/finalProject.zip/weatherapi.js
@@ -32,51 +32,56 @@ function initCityDropdown() {
 function getWeather() {
     for (let city of cities) {
         if (city.name == cityDropdown.value) {
-            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.latitude}&lon=${city.longitude}&appid=02994ce022cf3771383f59b05c819664`)
-                .then((response) => {
-                    // If server is down, let users know the API is unavailable
-                    if (!response.ok) {
-                        emptyFields();
-                        throw new Error('API is currently unavailable');
-                    }
-                    return response.json();
+            fetch(`https://api.weather.gov/points/${city.latitude},${city.longitude}`)
+                .then((response) => response.json())
+                .then(data => {
+                    fetch(data.properties.forecast)
+                        .then((response) => {
+                            // If server is down, let users know the api is unavailable
+                            if (response.status === 503) {
+                                emptyFields();
+                                throw new Error('API is currently unavailable');
+                            }
+                            return response.json();
+                        })
+                        .then((newData) => {
+                            displayWeather(newData);
+                            console.log(newData)
+                        });
                 })
-                .then((data) => {
-                    displayWeather(data);
-                    console.log(data);
-                });
         }
     }
 }
 
 function displayWeather(data) {
-    emptyFields();
+    emptyFields()
 
-    const currentWeather = data.current;
-    const forecast = data.daily[0];
+    for (let period of data.properties.periods) {
+        // ONLY GRAB THE FIRST PERIOD
+        if (period.number == 1) {
+            currentTemp = period.temperature;
+            currentUnit = period.temperatureUnit;
+            cityTemp.innerHTML = `${currentTemp}°${currentUnit}`
+            cityDescription.innerHTML = `Forecast: ${period.detailedForecast} <br><br>`
+            cityWinds.innerHTML = `Winds: ${period.windDirection} ${period.windSpeed}`
+        }
+    }
 
-    currentTemp = convertKelvinToDefault(currentWeather.temp);
-    currentUnit = "°C";
-    cityTemp.innerHTML = `${currentTemp}${currentUnit}`;
-    cityDescription.innerHTML = `Forecast: ${forecast.weather[0].description}<br><br>`;
-    cityWinds.innerHTML = `Winds: ${currentWeather.wind_speed} m/s`;
-
-    // You can access other weather data from 'forecast' and 'currentWeather' objects as needed
 }
 
 // Clears fields if API unavailable
 function emptyFields() {
-    cityTemp.innerHTML = "";
-    cityDescription.innerHTML = `
-        Sorry, currently can't find the details on this city. 
-        Please try again later or try another city!<br><br>
-        Enjoy this absolutely random picture from this city though!`;
-    cityWinds.innerHTML = "";
+    cityTemp.innerHTML = ``
+    cityDescription.innerHTML = 
+    `Sorry, currently can't find the details on this city. 
+    Please try again later or try another city! <br><br>
+    Enjoy this absolutely random picture from this city though!`
+    cityWinds.innerHTML = ``
 
     changeBg();
 }
 
-function changeBg() {
+function changeBg(){
     for (let city of cities) {
         if (city.name == cityDropdown.value) {
             cityNamePlaceholder.innerHTML = city.name;
@@ -87,25 +92,13 @@ function changeBg() {
 
 // Change the Current Temperature Unit When The cityTemp field is clicked
 function convertTemp() {
-    if (currentUnit === "°C") {
-        currentTemp = convertCelsiusToFahrenheit(currentTemp);
-        currentUnit = "°F";
-        cityTemp.innerHTML = `${currentTemp}${currentUnit}`;
+    if (currentUnit == "F") {
+        currentTemp = ((currentTemp - 32) * 5 / 9).toFixed(0)
+        currentUnit = "C"
+        cityTemp.innerHTML = `${currentTemp}°${currentUnit}`
     } else {
-        currentTemp = convertFahrenheitToCelsius(currentTemp);
-        currentUnit = "°C";
-        cityTemp.innerHTML = `${currentTemp}${currentUnit}`;
+        currentTemp = (currentTemp * 9 / 5 + 32).toFixed(0)
+        currentUnit = "F"
+        cityTemp.innerHTML = `${currentTemp}°${currentUnit}`
     }
-}
-
-function convertKelvinToDefault(temp) {
-    return (temp - 273.15).toFixed(0);
-}
-
-function convertCelsiusToFahrenheit(temp) {
-    return (temp * 9) / 5 + 32;
-}
-
-function convertFahrenheitToCelsius(temp) {
-    return ((temp - 32) * 5) / 9;
 }
